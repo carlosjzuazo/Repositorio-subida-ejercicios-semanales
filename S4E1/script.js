62b3f383b1f03b71fb8f8eda7b4d14166ad5f1e4
nanot <[carlitosjzuazo@gmail.com]>
2022-02-24 10:28:26 +0100

    Variables js

---
+++ S4E1/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = "Carlos";
+const age = 31;
+const favColor = "blanco";
+
+console.log(
+  `Hola, mi nombre es ${name}, tengo ${age} años y mi color favorito es el ${favColor}.`
+);
